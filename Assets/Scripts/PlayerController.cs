5e5452b506f17ea8f1e9609f6b90bec55e5db39d
daniel guindel <d.guindel.gonzalez@hotmail.com>
2020-04-14 12:55:25 +0200

    setup player controls

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float velocity = 0.1f;
+    public float jump = 3f;
+    
+    private Transform initialPosition;
+    public float positi;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        initialPosition = this.transform;
+        positi = initialPosition.localPosition.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+	    {
+            Debug.Log("Derecha");
+            this.transform.SetPositionAndRotation(new Vector3(this.transform.localPosition.x + velocity, this.transform.localPosition.y), this.transform.localRotation);
+    	}
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+	    {
+            Debug.Log("Izquierda");
+            this.transform.SetPositionAndRotation(new Vector3(this.transform.localPosition.x - velocity, this.transform.localPosition.y), this.transform.localRotation);
+	    }
+        
+        if (Input.GetKeyDown(KeyCode.Space) && this.transform.localPosition.y >= initialPosition.localPosition.y)
+	    {
+            Debug.Log("Salto");
+            this.transform.SetPositionAndRotation(new Vector3(this.transform.localPosition.x, this.transform.localPosition.y + jump), this.transform.localRotation);
+	    }
+        
+    }
+}
