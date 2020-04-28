ad13037977a5ea89468b6b872519dbc8b237cc60
Daniel <dguindelgonzalez-sobremesa@users.noreply.github.com>
2020-04-28 12:58:29 +0200

    obstacles to module 4

---
+++ Assets/Scripts/EnemyDamage.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyDamage : MonoBehaviour
+{
+    #region Private Fields
+
+    float nextDamage;
+
+    #endregion
+
+    #region Public properties
+
+    public float Damage;
+    public float DamageRate;
+    public float PushBackForce;
+
+    #endregion
+
+    #region Unity3D Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nextDamage = Time.time;
+    }
+
+    #endregion
+
+    #region Private Methods
+
+    void OnTriggerStay2D(Collider2D other)
+    {
+        if (other.tag == "Player" && nextDamage < Time.time)
+        {
+            PlayerHealth thePlayerHealth = other.gameObject.GetComponent<PlayerHealth>();
+            thePlayerHealth.AddDamage(Damage);
+            nextDamage = Time.time + DamageRate;
+            PushBack(other.transform);
+        }
+    }
+
+    void PushBack(Transform pushedObject)
+    {
+        Vector2 pushDirection = new Vector2(0, (pushedObject.position.y - transform.position.y)).normalized;
+        pushDirection *= PushBackForce;
+        Rigidbody2D pushRB = pushedObject.gameObject.GetComponent<Rigidbody2D>();
+        pushRB.velocity = Vector2.zero;
+        pushRB.AddForce(pushDirection, ForceMode2D.Impulse);
+    }
+
+    #endregion
+
+}
---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour
+{
+    #region Private Fields
+
+    float currentHealth;
+    AudioSource playerAS;
+    Color flashColour = new Color(255f, 255f, 255f, 0.5f);
+    float indicatorSpeed = 5f;
+    bool damaged;
+
+    #endregion
+
+    #region Public Properties
+
+    public Image DamageIndicator;
+    public CanvasGroup enGameCanvas;
+    public float FullHealth;
+    public AudioClip PlayerDamaged;
+
+    #endregion
+
+    #region Unity3D Methods
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHealth = FullHealth;
+        playerAS = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (damaged)
+        {
+            DamageIndicator.color = flashColour;
+        }
+        else
+        {
+            DamageIndicator.color = Color.Lerp(DamageIndicator.color, Color.clear, indicatorSpeed * Time.deltaTime);
+        }
+        damaged = false;
+    }
+
+    #endregion
+
+    #region Public Methods
+
+    public void AddDamage(float damage)
+    {
+        if (damage <= 0)
+            return;
+
+        currentHealth -= damage;
+        playerAS.PlayOneShot(PlayerDamaged);
+        damaged = true;
+
+        if (currentHealth <= 0)
+        {
+            MakeDead();
+        }
+    }
+
+    public void MakeDead()
+    {
+
+    }
+
+    #endregion
+}
